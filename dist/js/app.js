f27fc4fb2b355a64d2947bab23407bf1ced61450
Erik Shumkov <erikshumkov@gmail.com>
2020-08-16 21:57:28 +0200

    add search highlight

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,26 +1,100 @@
 import { data } from "./data.js"
 
 const content = document.getElementById("content");
+const landMenu = document.getElementById("landmenu");
+const search = document.getElementById("search")
+const options = []
+const values = []
 
-content.innerHTML = `
-  ${data.map(race => `
+
+for (let i = 0; i < data.length; i++) {
+  if (!options.includes(data[i].countryLong)) options.push(data[i].countryLong)
+
+  if (!values.includes(data[i].country)) values.push(data[i].country)
+}
+
+landMenu.innerHTML = `
+  <option value="all">Alla</option>
+  ${options.map((country, index) => `
+  <option value="${values[index]}">${country}</option>
+  `)}
+`
+
+function listContent(arr) {
+
+  const regex = new RegExp(search.value, 'gi');
+
+  if (arr.length === 0) return content.innerHTML = `
+  <div class="race-item no-results">
+    <div class="event no-results">
+      Inga resultat för "${search.value}". <span class="clear-field">Rensa sökfältet</span>
+    </div>
+  </div>
+  `
+
+  return content.innerHTML = `
+  ${arr.map(race => `
   <div class="race-item">
   <div class="event">
-    ${race.name}
+    ${race.name.replace(regex, `<span class="hl">${search.value}</span>`)}
     <div class="date-mobile">
-      <span class="date-mobile__d">13 Aug, 2020</span>
-      <span class="flag-icon flag-icon-se"></span>
+      <span class="date-mobile__d">${race.date}</span>
+      <span class="flag-icon flag-icon-${race.country}"></span>
     </div>
   </div>
   <span class="location"
-    >${race.place} <span class="flag-icon flag-icon-${race.country}"></span
+    >${race.place.replace(regex, `<span class="hl">${search.value}</span>`)} <span class="flag-icon flag-icon-${race.country}"></span
   ></span>
   <span class="date">${race.date}</span>
-  <span class="price">${race.price} SEK</span>
+  <span class="price">${race.price} ${race.currency}</span>
   <a
     class="action"
     href="${race.link}"
     target="_blank">till hemsida</a>
   </div>
-  `).join("")}
-`
+  `).join("")}`
+}
+
+function updateList() {
+  const filterDataArr = data.filter(race => {
+
+    if (landMenu.value === "all") return race;
+
+    if (race.country === landMenu.value) return race;
+  })
+
+  listContent(filterDataArr);
+}
+
+function searchFilter() {
+  const filterRaces = data.filter(race => {
+    if (
+      race.name.toLocaleLowerCase().includes(search.value.toLocaleLowerCase())
+      ||
+      race.place.toLocaleLowerCase().includes(search.value.toLocaleLowerCase())
+    ) {
+      return race;
+    }
+  })
+
+  listContent(filterRaces);
+}
+
+function clearSearchField(e) {
+  if (e.target.className = "clear-field") {
+    search.value = "";
+    listContent(data);
+  }
+}
+
+// Initialize the page with list items
+listContent(data);
+
+// Event listeners
+landMenu.addEventListener("input", updateList)
+
+// Updates racelist with every keypress
+search.addEventListener("input", searchFilter)
+
+// Clears search field
+content.addEventListener("click", clearSearchField)
--- dist/js/data.js
+++ dist/js/data.js
@@ -1,17 +1,10 @@
 export const data = [
   {
-    name: "Backamo 12H",
-    place: "Ljungskile",
-    country: "se",
-    date: "13 Aug, 2020",
-    price: 1095,
-    link: "http://www.militaryfitnessweden.com/"
-  },
-
-  {
     name: "Next Challenge",
     place: "Kalmar",
     country: "se",
+    countryLong: "Sverige",
+    currency: "SEK",
     date: "29 Aug, 2020",
     price: 645,
     link: "https://www.nextchallenge.se/"
@@ -21,6 +14,8 @@ export const data = [
     name: "Fåle 9k",
     place: "Limmared",
     country: "se",
+    countryLong: "Sverige",
+    currency: "SEK",
     date: "19 Sep, 2020",
     price: 395,
     link: "https://worldofraces.com/events/20eddc-Figf-asle"
@@ -30,17 +25,76 @@ export const data = [
     name: "Tylösand OCR",
     place: "Halmstad",
     country: "se",
+    countryLong: "Sverige",
+    currency: "SEK",
     date: "19 Sep, 2020",
     price: 600,
     link: "http://www.tylosandocr.se/"
   },
 
   {
+    name: "Evolution Race Mountain",
+    place: "Hedehusene",
+    country: "dk",
+    countryLong: "Danmark",
+    currency: "DKK",
+    date: "26 Sep, 2020",
+    price: 349,
+    link: "http://evolutionrace.dk/"
+  },
+
+  {
     name: "Berzerk OCR",
     place: "Landskrona",
     country: "se",
+    countryLong: "Sverige",
+    currency: "SEK",
     date: "3 Okt, 2020",
     price: 695,
     link: "https://berzerk.se/"
+  },
+
+  {
+    name: "Run OCR",
+    place: "Karlshamn",
+    country: "se",
+    countryLong: "Sverige",
+    currency: "SEK",
+    date: "10 Okt, 2020",
+    price: 595,
+    link: "http://www.runocr.se/"
+  },
+
+  {
+    name: "Ragnarok Races",
+    place: "Skåne",
+    country: "se",
+    countryLong: "Sverige",
+    currency: "SEK",
+    date: "31 Okt, 2020",
+    price: 595,
+    link: "https://www.ragnarokraces.com/"
+  },
+
+  {
+    name: "Evolution Race MX",
+    place: "Copenhagen",
+    country: "dk",
+    countryLong: "Danmark",
+    currency: "DKK",
+    date: "21 Nov, 2020",
+    price: 349,
+    link: "http://evolutionrace.dk/"
+  },
+
+  {
+    name: "Winter Warrior Finland",
+    place: "Ylläs",
+    country: "fi",
+    countryLong: "Finland",
+    currency: "EUR",
+    date: "10 Apr, 2021",
+    price: 75,
+    link: "https://winterwarrior.fi/"
   }
 ]
